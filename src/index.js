a6da3ce851acadb4f7f349dab619f7c79aaa45a8
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2020-10-05 10:57:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr=[]
+  const opening=[]
+  const closing=[]  
+
+  bracketsConfig.forEach((el)=>{
+    el.forEach((subEl,i)=>{
+      if(i==0){
+        opening.push(subEl)
+      }else {
+        closing.push(subEl)
+      }
+    })
+  })
+
+  let flag = false
+  str.split('').forEach((el)=>{
+    if (opening.includes(el) && el != '|'  && el != '7' && el != '8'){
+      arr.push(el)
+      console.log(arr)
+    }else if(el=='|' && !flag || el=='7' && !flag || el=='8' && !flag){
+      flag = true
+      arr.push(el)
+    } else if(closing.includes(el)){
+    const pair = opening[closing.indexOf(el)]
+    if (arr[arr.length-1] == pair ){
+      arr.pop()
+    }else {
+      arr.push(el)
+    }
+  }
+})
+return(arr.length==0)
 }
